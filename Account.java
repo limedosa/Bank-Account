bcdb9daf5d150e4a58397a5b2e48540a35e845e5
sophieelin <56371476+sophieelin@users.noreply.github.com>
2023-10-12 19:53:59 -0400

    checking account and account
    coded toString and withdraw in checking, and deposit, withdraw, tostring in account

---
+++ Account.java
@@ -0,0 +1,31 @@
+/**
+ * Represents a Bank Account
+ *
+ *  @author Sophie Lin and Linda Dominguez
+ * @version October 12, 2023
+ */
+public abstract class Account
+{
+    protected double balance;
+    protected int accountNum;
+    /**
+     * Withdraws the given amount form the checking account and incurs a fee if the balance is below the minimum
+     *
+     * @param  amt  the amount of money to be withdrawn
+     */
+    public abstract void withdraw(double amt);
+    /**
+     * Deposit the given amount into the checking account
+     *
+     * @param  amt  the amount of money to be deposited into account
+     */
+    public void deposit(double amt){
+        balance += amt;
+    }
+    /**
+     * Print out the details of the bank account
+     *
+     * @return  the String representation of the ] Account
+     */
+    public abstract String toString();
+}
---
+++ CheckingAccount.java
@@ -0,0 +1,37 @@
+
+/**
+ * Represents a Checking Account.
+ *
+ * @author Sophie Lin and Linda Dominguez
+ * @version October 12, 2023
+ */
+public class CheckingAccount extends Account
+{
+    private final double min = 100;
+    private final double fee = 25;
+
+    /**
+     * Withdraws the given amount form the checking account and incurs a fee if the balance is below the minimum
+     *
+     * @param  amt  the amount of money to be withdrawn
+     */
+    public void withdraw(double amt)
+    {
+        if(amt > this.balance)
+            System.out.println("** Withdrawal DENIED **: Insufficient balance.");
+        else{
+            this.balance -= amt;
+            if(this.balance < min)
+                this.balance -= 25;
+        }
+    }
+    /**
+     * Prints out the details of the Checking Account
+     *
+     * @return  the String representation of the Checking Account
+     */
+    public String toString()
+    {
+        return ("Checking account num: "+ this.accountNum+ "\tBalance: $"+ this.balance+"\tMinimum balance: " + min+ "\tOverdraft fee:" + fee);
+    }
+}
