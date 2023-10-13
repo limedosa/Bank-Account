d89a549b765b3d1e9078b32b086624ee7f8e1313
sophieelin <56371476+sophieelin@users.noreply.github.com>
2023-10-12 21:35:49 -0400

    changed ccheckingaccount sconstructor to bank main() and changed bank counter method

--- Account.java
+++ Account.java
@@ -13,31 +13,8 @@ public abstract class Account
     protected int accountNum;
     public String accountName;
     public Account(){
-        Scanner newScan = new Scanner (System.in);
-        System.out.println("Enter the name of this account: ");
-        String newAccountName=newScan.next();
-        this.accountName = newAccountName;
-        System.out.println("Welcome to x Bank! /n There are two types of accounts; checking and savings. \nDo you want to open a checking account?(y/n)");
-        String checking = newScan.nextLine();
-        //if user said yes then make checking 
-        int newAccountNum = makeAccNum();
-        if (checking.contains("y")){
-            //make checking account TODO change after method is defined
-            CheckingAccount newChecking = new CheckingAccount(newAccountNum);
-            System.out.println("Checking account successfully created!");
-    }
-        System.out.println("Great! \nDo you want to open a savings account?(y/n)");
-        String savings = newScan.nextLine();
-        //if user said yes then make checking 
-        if (savings.contains("y")){
-            //make savings account  -- TODO change after method is defined
-            SavingsAccount newSavings = new SavingsAccount(newAccountNum);
-            System.out.println("Checking account successfully created!\n Welcome to our Bank, "+ accountName  + ".");
 
     }
-    // public static void MINBALANCE
-    
-}
     /**
      * helper class allows for us to make random account number
      * @param None 
--- Bank.java
+++ Bank.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Represents a Bank
@@ -7,7 +8,7 @@
  */
 public class Bank {
     private Account[] bank;
-    private int num = 0;
+    private int count = 0;
 
     /**
      * Constructor for a Bank that creates 10 possible spaces for accounts
@@ -16,12 +17,17 @@ public class Bank {
         bank = new Account[10];
     }
     /**
-     * Adds an account to the bank
+     * Adds an account to the bank, and sets the account number
      * @param x Account to add to the bank
      */
     public void add(Account x){
-        if(num < bank.length)
-            bank[num] = x;
+        if(count < bank.length) {
+            bank[count] = x; //adds the account to the bank collection
+            x.accountNum = count; //adds the account number to the account object
+            count++;
+        }
+        else
+            System.out.println("No more space in this Bank");
     }
     /**
      * Finds the account given the number
@@ -54,6 +60,27 @@ public class Bank {
         return total;
     }
     public static void main(String[] args){
+        Scanner newScan = new Scanner (System.in);
+        System.out.println("Enter the name of this account: ");
+        String newAccountName=newScan.next();
+        this.accountName = newAccountName;
+        System.out.println("Welcome to x Bank! /n There are two types of accounts; checking and savings. \nDo you want to open a checking account?(y/n)");
+        String checking = newScan.nextLine();
+        //if user said yes then make checking
+        int newAccountNum = makeAccNum();
+        if (checking.contains("y")){
+            //make checking account TODO change after method is defined
+            CheckingAccount newChecking = new CheckingAccount(newAccountNum);
+            System.out.println("Checking account successfully created!");
+        }
+        System.out.println("Great! \nDo you want to open a savings account?(y/n)");
+        String savings = newScan.nextLine();
+        //if user said yes then make checking
+        if (savings.contains("y")){
+            //make savings account  -- TODO change after method is defined
+            SavingsAccount newSavings = new SavingsAccount(newAccountNum);
+            System.out.println("Checking account successfully created!\n Welcome to our Bank, "+ accountName  + ".");
 
+        }
     }
 }
