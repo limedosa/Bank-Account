3a6e8d3e572be05873ec43a25f0ba596819ed6c0
limedosa <109569567+limedosa@users.noreply.github.com>
2023-10-12 20:16:07 -0400

    combined acc

--- Account.java
+++ Account.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Represents a Bank Account
  *
@@ -8,6 +10,26 @@ public abstract class Account
 {
     protected double balance;
     protected int accountNum;
+    public Account(){
+        Scanner newScan = new Scanner (System.in);
+        System.out.println("Welcome to x Bank! /n There are two types of accounts; checking and savings. \nDo you want to open a checking account?(y/n)");
+        String checking = newScan.nextLine();
+        //if user said yes then make checking 
+        if (checking.contains("y")){
+            //make checking account TODO change after method is defined
+            CheckingAccount newChecking = new CheckingAccount();
+    }
+        System.out.println("Great! \nDo you want to open a savings account?(y/n)");
+        String savings = newScan.nextLine();
+        //if user said yes then make checking 
+        if (savings.contains("y")){
+            //make savings account  -- TODO change after method is defined
+            SavingsAccount newSavings = new SavingsAccount();
+    }
+    // public static void MINBALANCE
+    
+}
+
     /**
      * Withdraws the given amount form the checking account and incurs a fee if the balance is below the minimum
      *
